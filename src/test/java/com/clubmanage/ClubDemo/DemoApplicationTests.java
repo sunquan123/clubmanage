db8c9f873229608adca745d501768a7c4f2e2f2c
孙权 <sunquan@sunquandeMacBook-Pro.local>
2020-02-06 21:48:42 +0800

    增加单元测试

--- src/test/java/com/clubmanage/ClubDemo/DemoApplicationTests.java
+++ src/test/java/com/clubmanage/ClubDemo/DemoApplicationTests.java
@@ -1,11 +1,14 @@
 package com.clubmanage.ClubDemo;
 
+import com.clubmanage.ClubDemo.Pojo.Club;
 import com.clubmanage.ClubDemo.config.testConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
 
 @SpringBootTest
 @RunWith(SpringRunner.class )
@@ -15,6 +18,18 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("asdada"+testConfig.getTestMap());
+    }
+
+    @Test
+    void stringFormatTest(){
+        Club club=new Club();
+        club.setName("mike");
+        club.setManager("junior");
+        club.setCreateDate(new Date(121212));
+        club.setActivitytimes(1);
+        club.setMembercount(1);
+        String test=String.format("%s :%s",club.getManager(),club.getName());
+        System.out.println(test);
     }
 
 }
